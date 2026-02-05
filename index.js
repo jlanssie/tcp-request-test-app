8ede0247b145e7358fbe79c704e1d1b7f94e1616
Jeremy Lanssiers <jlanssie@gmail.com>
2026-02-05 21:13:04 +0100

    support local override for config and payload

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import net from 'node:net';
+import fs from 'node:fs';
 
-import config from './config.json' with { type: 'json' };
-import payload from './payload.json' with { type: 'json' };
+let config;
+if (fs.existsSync('./config.local.json')) {
+  config = JSON.parse(fs.readFileSync('./config.local.json', 'utf-8'));
+} else {
+  config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+}
+
+let payload;
+if (fs.existsSync('./payload.local.json')) {
+  payload = JSON.parse(fs.readFileSync('./payload.local.json', 'utf-8'));
+} else {
+  payload = JSON.parse(fs.readFileSync('./payload.json', 'utf-8'));
+}
 
 const client = new net.Socket();
 
